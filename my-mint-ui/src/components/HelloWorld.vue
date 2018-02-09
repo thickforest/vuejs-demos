c6a01c4671ced753244db5ffd4f948b9d07af99b
lvzhl <lvzhonglin@reining.com.cn>
2018-02-09 11:54:21 +0800

    测试了mint-ui的几个组件

--- my-mint-ui/src/components/HelloWorld.vue
+++ my-mint-ui/src/components/HelloWorld.vue
@@ -1,85 +1,16 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <mt-button type="primary" @click="pop_toast"> 选择操作 </mt-button>
+    <mt-button type="primary" @click="pop_indicator"> 加载 </mt-button>
+    <mt-button type="primary" @click="alert"> Alert </mt-button>
+    <mt-cell title="标题文字" value="说明文字"/>
+    <mt-field label="用户名" placeholder="请输入用户名" v-model="username"></mt-field>
+    <mt-field label="密码" placeholder="请输入密码" type='password' v-model="password"></mt-field>
+    <mt-field label="邮箱" placeholder="请输入邮箱" type='email' state='error' v-model="email"></mt-field>
+    <mt-field label="日期" placeholder="请输入日期" type='date' v-model="form_date"></mt-field>
+    <mt-field label="文件" placeholder="请选择文件" type='file' v-model="filename"></mt-field>
+    <mt-switch v-model="value">开关</mt-switch>
   </div>
 </template>
 
@@ -88,26 +19,35 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      value: true,
+      username: 'forest',
+      password: 'xxx',
+      email: '',
+      form_date: '',
+      filename: ''
+    }
+  },
+  methods: {
+    pop_toast: function () {
+      this.$toast({
+        message: 'hello, world!!',
+        iconClass: 'icon icon-success'
+      })
+    },
+    pop_indicator: function () {
+      this.$indicator.open({
+        text: 'hello',
+        spinnerType: 'fading-circle'
+      })
+    },
+    alert: function () {
+      this.$messagebox.confirm('确定？').then(action => {
+        console.log('确定' + action)
+      }).catch(action => {
+        console.log('取消' + action)
+      })
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
