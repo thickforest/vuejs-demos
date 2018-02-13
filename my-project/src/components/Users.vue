657616420124ff2a9b05e2f98eab481efebf2cc9
root <root@acer>
2018-02-14 05:57:15 +0800

    学习： (1) window.URL.createObjectURL(this.$refs.avatar.files[0]) 实现本地图片预览 (2) vue中<label for 可以隐藏掉丑的上传输入框 (3) 可以通过new FileReader() readAsDataURL 读取本地预上传文件为base64编码

--- my-project/src/components/Users.vue
+++ my-project/src/components/Users.vue
@@ -2,11 +2,12 @@
   <div class="jumbotron">
     <h1>用户列表</h1>
     <div class="row">
-      <div class="col-md-6">
+      <div class="col-md-12">
         <table class="table table-hover">
           <thead>
             <tr>
               <th>#</th>
+              <th>头像</th>
               <th>用户名</th>
               <th>密码</th>
               <th>年龄</th>
@@ -15,6 +16,7 @@
           </thead>
           <tbody v-for="(user,index) in users" v-bind:key="user.id">
             <tr>
+              <td></td>
               <td>{{ user.id }}</td>
               <td>{{ user.username }}</td>
               <td>{{ user.password }}</td>
@@ -25,6 +27,12 @@
           <tbody>
             <tr>
               <td>#</td>
+              <td>
+                <label for="avatarId" class="btn btn-default">更换头像</label>
+                <input id="avatarId" type="file" style="display:none" @change="avatarChange" ref="avatar" placeholder=""/>
+                <img :src="avatarLocalURL" class="img-circle img-responsive" />
+                <label v-if="avatarFile!=null">{{ avatarFile.name }} {{ avatarFile.size}}</label>
+              </td>
               <td><input type="text" class="form-control" v-model="userForm.username" placeholder=""/></td>
               <td><input type="password" class="form-control" v-model="userForm.password" placeholder=""/></td>
               <td><input type="number" class="form-control" v-model="userForm.age" placeholder=""/></td>
@@ -49,6 +57,9 @@ export default {
         password: '',
         age: 30
       },
+      avatarFile: null,
+      avatarLocalURL2: 'https://sfault-avatar.b0.upaiyun.com/147/223/147223148-573297d0913c5_huge256',
+      avatarLocalURL: 'http://192.168.123.33:90/erzi.jpeg',
       users: []
     }
   },
@@ -56,7 +67,7 @@ export default {
     getUsers () {
       axios({
         method: 'GET',
-        url: 'http://localhost:5000/user'
+        url: 'http://192.168.123.33:5000/user'
       }).then((res) => {
         this.users = res.data.data
       })
@@ -64,7 +75,7 @@ export default {
     delUser (id, index) {
       axios({
         method: 'DELETE',
-        url: 'http://localhost:5000/user/' + id
+        url: 'http://192.168.123.33:5000/user/' + id
       }).then((res) => {
         this.users.splice(index, 1)
       })
@@ -72,12 +83,27 @@ export default {
     addUser () {
       axios({
         method: 'POST',
-        url: 'http://localhost:5000/user',
+        url: 'http://192.168.123.33:5000/user',
         data: this.userForm
       }).then((res) => {
         this.users.push({'id': res.data.data.id, 'username': this.userForm.username, 'password': this.userForm.password, 'age': this.userForm.age})
         this.userForm = { username: '', password: '', age: 30 }
       })
+    },
+    avatarChange () {
+      this.avatarFile = this.$refs.avatar.files[0]
+      console.log(this.avatarFile)
+      if (typeof FileReader === 'undefined') {
+        alert('FileReader not support!!!')
+        return
+      }
+      this.avatarLocalURL = window.URL.createObjectURL(this.avatarFile) // blob:http://192.168.123.33:8080/0eeb05fb-2389-433e-b63b-39bf64673218
+      var reader = new FileReader()
+      reader.onload = function (e) {
+        // this.avatarLocalURL = e.target.result // data:image/jpeg;base64,xxxxxxxxxxx
+        // console.log(e)
+      }
+      reader.readAsDataURL(this.avatarFile)
     }
   }
 }
